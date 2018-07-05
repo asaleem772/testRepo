761d888013357b5884348a1c2b0bc9fe0fd17f6c
asaleem772 <shahzada.ali.saleem@gmail.com>
2018-07-05 13:53:42 +0100

    push2 - test

--- src/main/main.go
+++ src/main/main.go
@@ -6,4 +6,5 @@ import (
 
 func main() {
 	fmt.Println("Yahoo ! Git Set")
+	//	user.helloUser()
 }
---
+++ src/user/user.go
@@ -0,0 +1,9 @@
+package user
+
+import (
+	"fmt"
+)
+
+func helloUser() {
+	fmt.Println("User Says Hello !!")
+}
